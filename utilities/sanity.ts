93eb6ebc7cac9e63ed02ca3a400dc521472d1784
Bryn Dyment <bryndyment@gmail.com>
2023-11-14 11:09:41 +0900

    update

--- utilities/sanity.ts
+++ utilities/sanity.ts
@@ -4,7 +4,7 @@ import { createClient } from 'next-sanity'
 
 // constants
 
-export const SANITY_CLIENT = createClient({ apiVersion: '2023-11-14', dataset: DATASET, projectId: PROJECT_ID, useCdn: process.env.NODE_ENV === 'production' })
+export const SANITY_CLIENT = createClient({ apiVersion: '2023-11-14', dataset: DATASET, projectId: PROJECT_ID, useCdn: false })
 
 // functions
 
