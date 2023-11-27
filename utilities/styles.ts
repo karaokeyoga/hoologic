717e7bd377155b8dfccd77dbf080943ccb2fa97b
Bryn Dyment <bryndyment@gmail.com>
2023-11-27 10:18:37 +0900

    update

--- utilities/styles.ts
+++ utilities/styles.ts
@@ -8,7 +8,8 @@ export const BREAKPOINTS = [320, 640, 960]
 export const SX_CONTENT = {
   a: { color: BLACK },
   img: { display: 'block', height: 'auto', width: '100%' },
-  'li, p': { ':last-child': { mb: 0 }, lineHeight: 1.4, mb: 0.875, mt: 0 }
+  'li, p': { ':last-child': { mb: 0 }, lineHeight: 1.4, mb: 0.875, mt: 0 },
+  ul: { listStyleType: 'none', my: 0, pl: 0 }
 }
 
 export const WHITE = common.white
