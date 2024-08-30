7cc5b14eee0550a9d3040cb049271ee4058c30b3
Bryn Dyment <bryndyment@gmail.com>
2024-08-30 16:22:47 +0900

    eslint config tweaks (#453)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,24 @@
 import { fixupPluginRules } from '@eslint/compat'
 import { FlatCompat } from '@eslint/eslintrc'
-import eslint from '@eslint/js'
+import eslintJs from '@eslint/js'
 import eslintPluginNext from '@next/eslint-plugin-next'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import eslintPluginPerfectionist from 'eslint-plugin-perfectionist'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import eslintPluginReact from 'eslint-plugin-react'
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 import path from 'node:path'
 import url from 'node:url'
-import tseslint from 'typescript-eslint'
+import typescriptEslint from 'typescript-eslint'
 
 export default [
-  eslint.configs.recommended,
+  eslintJs.configs.recommended,
+  ...new FlatCompat({ baseDirectory: path.dirname(url.fileURLToPath(import.meta.url)) }).extends('eslint-config-standard'),
   eslintPluginPerfectionist.configs['recommended-natural'],
-  eslintPluginPrettierRecommended,
   eslintPluginReact.configs.flat.recommended,
-  ...new FlatCompat({
-    baseDirectory: path.dirname(url.fileURLToPath(import.meta.url))
-  }).extends('eslint-config-standard'),
-  ...tseslint.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
   {
     plugins: {
       '@next/next': fixupPluginRules(eslintPluginNext),
@@ -26,14 +26,8 @@ export default [
     },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      'no-unused-vars': 'off',
-      'no-use-before-define': 'off',
       'perfectionist/sort-imports': ['error', { newlinesBetween: 'never' }],
       'prettier/prettier': ['error', { arrowParens: 'avoid', endOfLine: 'lf', printWidth: 160, semi: false, singleQuote: true, trailingComma: 'none' }],
-      'react/display-name': 'off',
-      'react/jsx-uses-react': 'off',
-      'react/prop-types': 'off',
       'react/react-in-jsx-scope': 'off',
       ...eslintPluginNext.configs.recommended.rules,
       '@next/next/no-img-element': 'off',
